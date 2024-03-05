73c19c978ac5861cb4f6c17308de07e492b1ed59
TRAORE-07 <traoredjeneba0711@gmail.com>
2024-03-05 16:49:08 +0530

    New solutions added.

---
+++ Easy/Print_Function.py
@@ -0,0 +1,6 @@
+if __name__ == '__main__':
+    n = int(input())
+    for i in range(1, n+1):
+        #The end='' argument to the print() function tells Python
+        # to print each number without adding a newline character after it.
+        print(i, end='')
